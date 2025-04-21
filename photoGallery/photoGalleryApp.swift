7f61f8d9c01b56f7323bbebfba2ffeb6b3b1888a
shafiamanzoor762 <shafiamanzoor762@gmail.com>
2025-04-21 22:27:04 +0500

    Initial Commit

---
+++ photoGallery/photoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoGalleryApp.swift
+//  photoGallery
+//
+//  Created by apple on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct photoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
